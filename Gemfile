0a03237404ecdbb76a0d3665a0097b054e8633c8
rodmal52 <rodrigo@skinnystork.com>
2014-10-14 13:53:39 -0500

    initial

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
+gem 'bitters'
+gem 'bourbon'
+gem 'neat'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -37,4 +40,3 @@ gem 'rest-client'
 
 # Use debugger
 # gem 'debugger', group: [:development, :test]
-
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,35 +2,95 @@ require 'rest-client'
 require "cgi"
 require 'logger'
 require 'active_support/json'
+require 'json'
 
 class HomeController < ApplicationController
   def index
-  	@url = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name'
+      @api_key = 'api_key=fd609a75-8aa4-4242-b09c-10c11e722804'
+    def getSummoner(name)
+      @url = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name'
+      endpoint = name
+      query = ::CGI::escape(endpoint)
+      params = @api_key
+      url_components = [@url, endpoint]
+      @url =  "#{url_components.join('/')}?#{params}"
+      header = {}
+        begin
+          @response = RestClient.get @url, header
+        rescue Exception => e
+          logger.error "==================================================="
+          logger.debug "An exception occured while processing GET request to url: #{@url}"
+          logger.error e.to_s
+          logger.error "==================================================="
+          @response = { error: e.message}.to_json
+        end
+        @response= JSON.parse(@response.body)[name.downcase]
+    end
+
+    def get_games(summonerID)
+      @url = 'https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner'
+
+      endpoint = summonerID.to_s
+      query = ::CGI::escape(endpoint)
+      params = @api_key
+      url_components = [@url, endpoint]
+      @url =  "#{url_components.join('/')}/recent?#{params}"
+      @url2 = @url
+      header = {}
+        begin
+          @response = RestClient.get @url, header
+        rescue Exception => e
+          logger.error "==================================================="
+          logger.debug "An exception occured while processing GET request to url: #{@url}"
+          logger.error e.to_s
+          logger.error "==================================================="
+          @response = { error: e.message}.to_json
+        end
+        @response= JSON.parse(@response.body)['games']
+
+
+
+    end
+=begin
+    def match_history(name)
+      @url = 'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory'
+      endpoint = name
+      query = ::CGI::escape(endpoint)
+      params = @api_key
+      url_components = [@url, endpoint]
+      @url =  "#{url_components.join('/')}?#{params}"
+      header = {}
+        begin
+          @response = RestClient.get @url, header
+        rescue Exception => e
+          logger.error "==================================================="
+          logger.debug "An exception occured while processing GET request to url: #{@url}"
+          logger.error e.to_s
+          logger.error "==================================================="
+          @response = { error: e.message}.to_json
+        end
+        @response
+
+    end
+
+    if params[:search]
+      @result = getSummoner(:search)
+      @id = getSummoner(:search)['id']
+      @name = getSummoner(:search)['name']
+      @profileIconId = getSummoner(:search)['profileIconId']
+      @summonerLevel = getSummoner(:search)['summonerLevel']
+
+    else
+=end
+      @result = getSummoner('RodMal')
+      @id = getSummoner('RodMal')['id']
+      @name = getSummoner('RodMal')['name']
+      @profileIconId = getSummoner('RodMal')['profileIconId']
+      @summonerLevel = getSummoner('RodMal')['summonerLevel']
+
+    #end
+      @games = get_games(@id)
 
   	#@url= 'https://na.api.pvp.net/api/lol/na/v1.2'
-  	@api_key = 'api_key=fd609a75-8aa4-4242-b09c-10c11e722804'
-  	endpoint = 'RodMal'
-  	query = ::CGI::escape(endpoint)
-	params = @api_key
-	
-	
-
-    url_components = [@url, endpoint]
-	
-    @url =  "#{url_components.join('/')}?#{params}"
-    
-    header = {}
-      begin
-        @response = RestClient.get @url, header
-      rescue Exception => e
-        logger.error "==================================================="
-        logger.debug "An exception occured while processing GET request to url: #{@url}"
-        logger.error e.to_s
-        logger.error "==================================================="
-        @response = { error: e.message}.to_json
-      end
-
-      @response
-
   end
 end
